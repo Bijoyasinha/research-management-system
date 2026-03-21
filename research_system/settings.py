9b614fbef0257341497fced8e3fbb9a7d6ef59d1
Bijoyasinha <bijoyas783@gmail.com>
2026-03-21 13:18:52 +0600

    Deployment setup ready

--- research_system/settings.py
+++ research_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&!b#llb-rwoo*f7iw4h4&ejss&)$=mx0q4n_=3+qpmtkq-93+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
